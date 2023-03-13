c6170c1b3bf045f97c4ad28acc421f138c2ca3a9
Sebastián Garrido <sebgarridor@gmail.com>
2023-03-13 17:28:25 -0300

    testing onsubmit function

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class SelectComponent {
   username: string = '';
   period: string = 'overall';
+
+  onSubmit() {
+    console.log(this.username);
+    console.log(this.period);
+    
+  }
 }
